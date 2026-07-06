d3d499fb2ed00011de18be41ec059b8d96fa8e1b
hassantouheed <100044263+hassantouheed@users.noreply.github.com>
2026-07-06 07:10:39 -0400

    6 july commit

--- file1.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I will see the image of dragon cowsay"  >> cowsay1.txt
-grep -i "dragon" cowsay1.txt
-cat cowsay1.txt
-ls -l
